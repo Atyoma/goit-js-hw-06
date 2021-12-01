55612cea2388f8f004b9182d330cc966b7a8509b
Artem Kravchenko <atyoma.kravchenko@gmail.com>
2021-12-01 23:17:55 +0200

    add task 2

--- js/task-01.js
+++ js/task-01.js
@@ -9,29 +9,3 @@ listWithItem.forEach((element) => {
   console.log(`Category : ${titleEl.textContent}`);
   console.log(`Elements : ${numberEl.length}`);
 });
-
-// Задание 2
-
-// Напиши скрипт, который для каждого элемента
-// массива ingredients:
-
-// // Создаст отдельный элемент <li>. Обзательно
-// используй
-// // метод
-// // document.createElement().
-// // Добавит название ингредиента как его текстовое
-// // содержимое.
-// // Добавит элементу класс item.
-// // После чего вставит все <li> за одну операцию
-// // в список ul.ingredients.
-
-const ingredients = [
-  "Potatoes",
-  "Mushrooms",
-  "Garlic",
-  "Tomatos",
-  "Herbs",
-  "Condiments",
-];
-
-const addIngredient = ingredients.map((ingredient) => ingredient);
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,37 @@
+// Задание 2
+
+// Напиши скрипт, который для каждого элемента
+// массива ingredients:
+
+// // Создаст отдельный элемент <li>. Обзательно
+// используй
+// // метод
+// // document.createElement().
+// // Добавит название ингредиента как его текстовое
+// // содержимое.
+// // Добавит элементу класс item.
+// // После чего вставит все <li> за одну операцию
+// // в список ul.ingredients.
+
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ulEl = document.getElementById("ingredients");
+
+const addIngredient = ingredients.map((name) => {
+  const li = document.createElement("li");
+  li.classList.add("item");
+  li.textContent = name;
+
+  return li;
+});
+
+console.log(...addIngredient);
+
+ulEl.append(...addIngredient);
